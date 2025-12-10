db5bc14fe9223320f43b958ca5d7a142c71ca5bc
Abid Shahriar <121728537+CodewithShahriar@users.noreply.github.com>
2025-12-11 02:08:50 +0600

    display the sum of the number

---
+++ sum+.asm
@@ -0,0 +1,48 @@
+"Write an 8086 assembly language program (using emu8086) that prompts the user for two single-digit numbers, displays the entered numbers separated by a '+' and '=', and then calculates and displays their single-digit sum."
+
+
+
+; Add two numbers
+; It will take input from users
+; Display the sum of the Numbers
+
+
+.model small
+.stack 100h
+.data
+
+addition_sign db ' + $'
+equal_sign db ' = $'
+
+.code 
+
+mov ax, @data
+mov ds, ax
+
+
+mov ah, 1
+INT 21h
+
+mov bl, al
+
+mov ah, 9
+lea dx, addition_sign  
+INT 21h
+
+
+mov ah, 1
+INT 21h
+
+mov cl, al
+
+mov ah, 9
+lea dx, equal_sign
+int 21h
+
+add bl, cl
+sub bl, 30h
+
+mov ah, 2
+mov dl, bl
+int 21h
+
