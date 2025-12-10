fd51761cc52ff77b463f7f3c0d183231a2a876a2
Abid Shahriar <121728537+CodewithShahriar@users.noreply.github.com>
2025-12-11 02:02:28 +0600

    exlpain even odd details

--- even_odd.asm
+++ even_odd.asm
@@ -74,3 +74,69 @@ mov ah, 4Ch
 int 21h
 
 
+
+
+
+////////////////////
+Explanation
+///////////////////
+
+.model small
+.stack 100h
+.data
+
+even_msg db 'Number is Even.$'
+odd_msg db 'Number is Odd.$'    
+    
+.code
+
+mov ax, @data
+mov ds, ax      ; Initialize the Data Segment Register
+
+mov ah, 1
+int 21h         ; Read character input (ASCII in AL)
+
+sub al, 30h     ; Convert ASCII to numeric value (e.g., '5' (35H) -> 05H)
+                ; Now AL has the real number (0-9)
+
+mov bl, 2       ; Set divisor = 2
+div bl          ; AL / BL. AL = quotient, AH = remainder
+                ; If AH = 0 (Remainder is zero) -> even, else odd
+
+cmp ah, 0       ; Compare remainder (AH) with 0
+je even         ; Jump Equal: If AH = 0, jump to 'even'
+jne odd         ; Jump Not Equal: If AH != 0, jump to 'odd' (or just 'jmp odd' if no other code follows)
+
+
+even:           
+; Line Break sequence (0Ah/0Dh)
+mov ah, 2
+mov dl, 0Ah
+int 21h
+mov ah, 2
+mov dl, 0Dh
+int 21h 
+
+mov ah, 9       ; Display string function
+lea dx, even_msg
+int 21h
+jmp end
+
+odd:            
+; Line Break sequence (0Ah/0Dh)
+mov ah, 2
+mov dl, 0Ah
+int 21h
+mov ah, 2
+mov dl, 0Dh
+int 21h
+
+mov ah, 9       ; Display string function
+lea dx, odd_msg
+int 21h
+jmp end
+
+
+end:
+mov ah, 4Ch     ; Terminate Program
+int 21h
