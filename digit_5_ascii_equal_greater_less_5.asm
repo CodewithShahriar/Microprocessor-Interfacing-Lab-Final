43c78403633b5929606bc45db764b1bddd852daa
Abid Shahriar <121728537+CodewithShahriar@users.noreply.github.com>
2025-12-11 02:58:22 +0600

    5 equal less greater

--- digit_5_ascii_equal_greater_less_5.asm
+++ digit_5_ascii_equal_greater_less_5.asm
@@ -69,3 +69,69 @@ int 21h
 
 
 
+
+
+///////////////
+
+Explanation
+
+////////////////
+
+.model small
+.stack 100h
+.data
+ 
+input db 'Take an input : $'      ; Prompt for user
+sent1 db 'User input is equal to 5$'
+sent2 db 'User input is greater than 5$'
+sent3 db 'User input is less than 5$'    
+    
+.code 
+
+mov ax, @data
+mov ds, ax      ; Initialize the Data Segment Register
+
+mov ah, 9
+lea dx, input
+int 21h         ; Display the prompt "Take an input : "
+ 
+mov ah, 1
+int 21h         ; Read character input (ASCII in AL)
+
+mov cl, al      ; Move input ASCII (e.g., '6' = 36H) to CL
+mov bl, 35h     ; Store ASCII of '5' (35H) in BL
+           
+; Print Newline/Carriage Return
+mov ah, 2
+mov dl, 0Ah 
+int 21h
+mov ah, 2
+mov dl, 0Dh
+int 21h
+
+cmp cl, bl      ; Compare input character (CL) with '5' (BL)
+je equal        ; Jump Equal: If CL = BL, jump to 'equal'
+jg greater      ; Jump Greater: If CL > BL, jump to 'greater'
+jl lessthan     ; Jump Less: If CL < BL, jump to 'lessthan' 
+
+equal: 
+mov ah, 9
+lea dx, sent1
+int 21h         ; Display "User input is equal to 5"
+jmp end
+
+greater:
+mov ah, 9
+lea dx, sent2
+int 21h         ; Display "User input is greater than 5"
+jmp end
+ 
+lessthan: 
+mov ah, 9
+lea dx, sent3
+int 21h         ; Display "User input is less than 5"
+jmp end 
+
+end:
+mov ah, 4Ch     ; Terminate Program
+int 21h
